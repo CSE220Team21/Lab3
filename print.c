f9f0d3920fc817d8f1f0bac55ea844889de82118
MMayr15 <mmayr@asu.edu>
2014-03-20 11:47:46 -0700

    Initial commit of print.c

---
+++ print.c
@@ -0,0 +1,94 @@
+ //	Team 21 (https://github.com/CSE220Team21/Lab3)
+ //	Mike Mayr (MMayr15), Charlie Crow (LevenBravo)
+ //	3/19/14
+ //	CSE 220: T/Th 10:30am
+ //  print.c
+ //  Lab3
+ //
+ //  Created by Bryce Holton.
+ //  Copyright (c) 2014 Bryce Holton. All rights reserved. */
+#include "print.h"
+
+const char* const SYMBOL_STRINGS[] = {
+    "<no token>", "<IDENTIFIER>", "<NUMBER>", "<STRING>",
+    "^","*","(",")","-","+","=","[","]",":",";",
+    "<",">",",",".","/",":=","<=",">=","<>","..",
+    "<END OF FILE>", "<ERROR>",
+    "AND","ARRAY","BEGIN","CASE","CONST","DIV","DO","DOWNTO",
+    "ELSE","END","FILE","FOR","FUNCTION","GOTO","IF","IN",
+    "LABEL","MOD","NIL","NOT","OF","OR","PACKED","PROCEDURE",
+    "PROGRAM","RECORD","REPEAT","SET","THEN","TO","TYPE","UNTIL",
+    "VAR","WHILE","WITH",
+};
+
+static void print_page_header(char source_name[], char date[]);
+
+void print_line(char line[], char source_name_to_print[], char date_to_print[]) 
+{
+    char save_ch;
+    char *save_chp = NULL;
+    static int line_count = MAX_LINES_PER_PAGE;
+    
+    if (++line_count > MAX_LINES_PER_PAGE)  
+	{
+        print_page_header(source_name_to_print, date_to_print);
+        line_count = 1;
+    }
+	if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
+	{
+        save_chp = &line[MAX_PRINT_LINE_LENGTH];
+    }
+    if (save_chp)
+	{
+        save_ch = *save_chp;
+        *save_chp = '\0';
+    }
+    printf("%s", line);
+    if (save_chp) 
+	{
+        *save_chp = save_ch;
+    }
+}
+
+static void print_page_header(char source_name[], char date[])
+{
+    static int page_number = 0;
+        putchar(FORM_FEED_CHAR);
+    printf("Page    %d  %s  %s\n\n", ++page_number, source_name, date);
+}
+
+
+void print_token(Token *token)
+{
+	if( token -> c1 == IDENTIFIER 
+	{ 
+		printf( ">> %s%s", SYMBOL_STRINGS[1], token -> val );
+	}
+	else if( token -> c1 == NUMBER )
+	{
+		if( token -> t1 == INTEGER_LIT )
+		{
+			printf( ">> %s%s", SYMBOL_STRINGS[2], token -> val );
+		}
+		else if ( token -> t1 == REAL_LIT )
+		{
+			printf( ">> %s%s", SYMBOL_STRINGS[2], token -> val );
+		}
+	}
+    
+	else if( token -> c1 == STRING )
+	{
+		if( token -> t1 == STRING_LIT )
+		{
+			printf( ">> %s%s", SYMBOL_STRINGS[3], token -> val );
+		}
+		else
+		{
+			printf( ">> %s%s", SYMBOL_STRINGS[3], token -> val );
+		}
+	}
+    else //only run if no token exists
+	{
+		printf( ">> %s%s", SYMBOL_STRINGS[0], token -> val );
+	}
+}
