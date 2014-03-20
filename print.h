82c9f0506be37ee5520eecadd6feb6e64aefd026
MMayr15 <mmayr@asu.edu>
2014-03-20 11:48:14 -0700

    Initial commit of print.h

---
+++ print.h
@@ -0,0 +1,18 @@
+ //	Team 21 (https://github.com/CSE220Team21/Lab3)
+ //	Mike Mayr (MMayr15), Charlie Crow (LevenBravo)
+ //	3/19/14
+ //	CSE 220: T/Th 10:30am
+ //  print.h
+ //  Lab3
+ //
+ //  Created by Bryce Holton.
+ //  Copyright (c) 2014 Bryce Holton. All rights reserved. */
+
+#ifndef Lab3_print_h
+#define Lab3_print_h
+#include "common.h"
+
+void print_line(char *, char source_name[], char date_to_print[]);
+void print_token(Token *token);
+
+#endif
